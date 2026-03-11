7ffeb03969bc88ab72f0a0b64ddc2ce9a3ebbb76
eliasfong2007-bit <eliasfong2007@gmail.com>
2026-03-10 21:36:47 -0400

    Changed baseURL
    Now it links to my Github

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://github.com/eliasfong2007-bit",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
